dd5f2392a4c1c889dfc948a0d3ebb918c3bad9d0
Jil Patel <jilpatel0511@gmail.com>
2021-06-02 07:57:37 +0530

    new file updated

--- Internship/Internship/asgi.py
+++ Internship/Internship/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Internship project.
+ASGI config for internship project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Internship.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internship.settings')
 
 application = get_asgi_application()
